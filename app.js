f75022b8fb814049ca90b49dbc3099961812c711
s-kimura <s-kimura@safie.jp>
2024-09-23 03:54:51 +0900

    fix MONGO_URL

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use("/api/v1/tasks", taskRoutes); // Use /api/v1/tasks as a prefix for all r
 // データベース接続
 const start = async () => { // 非同期処理を行う関数を定義
     try {
-        await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL); // Connect to the database
-        app.listen(process.env.PORT || PORT , console.log("Server is running on port", PORT)); // If connected, start the server
+        await connectDB("mongodb+srv://s-kimura:t2260622t@cluster0.17g8u.mongodb.net/TODO_App?retryWrites=true&w=majority&appName=Cluster0"); // Connect to the database
+        app.listen(process.env.PORT || PORT , console.log("Server is running")); // If connected, start the server
     } catch (error) {
         console.log(error);
     }
